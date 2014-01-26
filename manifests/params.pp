3c22449546aed9552d7b714e61fecff6b45d1096
Franz Kißig <franzk@s236048401.online.de>
2014-01-26 10:30:21 +0100

    Add ldap option allow_username_or_email_login

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,39 +1,39 @@
-
 # Class:: gitlab::params
 #
 #
 class gitlab::params {
 
-  $git_user               = 'git'
-  $git_home               = '/home/git'
-  $git_email              = 'git@someserver.net'
-  $git_comment            = 'GitLab'
-  $gitlab_sources         = 'git://github.com/gitlabhq/gitlabhq.git'
-  $gitlab_branch          = '6-1-stable'
-  $gitlabshell_sources    = 'git://github.com/gitlabhq/gitlab-shell.git'
-  $gitlabshell_branch     = 'v1.7.1'
-  $gitlab_dbtype          = 'mysql'
-  $gitlab_dbname          = 'gitlab'
-  $gitlab_dbuser          = 'gitlab'
-  $gitlab_dbpwd           = 'changeme'
-  $gitlab_dbhost          = undef
-  $gitlab_dbport          = undef
-  $gitlab_dbsocket        = undef
-  $gitlab_domain          = $::fqdn
-  $gitlab_repodir         = $git_home
-  $gitlab_ssl             = false
-  $gitlab_ssl_cert        = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
-  $gitlab_ssl_key         = '/etc/ssl/private/ssl-cert-snakeoil.key'
-  $gitlab_ssl_self_signed = false
-  $gitlab_projects        = '10'
-  $gitlab_username_change = true
-  $ldap_enabled           = false
-  $ldap_host              = 'ldap.domain.com'
-  $ldap_base              = 'dc=domain,dc=com'
-  $ldap_uid               = 'uid'
-  $ldap_port              = '636'
-  $ldap_method            = 'ssl'
-  $ldap_bind_dn           = ''
-  $ldap_bind_password     = ''
+  $git_user                           = 'git'
+  $git_home                           = '/home/git'
+  $git_email                          = 'git@someserver.net'
+  $git_comment                        = 'GitLab'
+  $gitlab_sources                     = 'git://github.com/gitlabhq/gitlabhq.git'
+  $gitlab_branch                      = '6-1-stable'
+  $gitlabshell_sources                = 'git://github.com/gitlabhq/gitlab-shell.git'
+  $gitlabshell_branch                 = 'v1.7.1'
+  $gitlab_dbtype                      = 'mysql'
+  $gitlab_dbname                      = 'gitlab'
+  $gitlab_dbuser                      = 'gitlab'
+  $gitlab_dbpwd                       = 'changeme'
+  $gitlab_dbhost                      = undef
+  $gitlab_dbport                      = undef
+  $gitlab_dbsocket                    = undef
+  $gitlab_domain                      = $::fqdn
+  $gitlab_repodir                     = $git_home
+  $gitlab_ssl                         = false
+  $gitlab_ssl_cert                    = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+  $gitlab_ssl_key                     = '/etc/ssl/private/ssl-cert-snakeoil.key'
+  $gitlab_ssl_self_signed             = false
+  $gitlab_projects                    = '10'
+  $gitlab_username_change             = true
+  $ldap_enabled                       = false
+  $ldap_host                          = 'ldap.domain.com'
+  $ldap_base                          = 'dc=domain,dc=com'
+  $ldap_uid                           = 'uid'
+  $ldap_port                          = '636'
+  $ldap_method                        = 'ssl'
+  $ldap_bind_dn                       = ''
+  $ldap_bind_password                 = ''
+  $ldap_allow_username_or_email_login = true
 
 } # Class:: gitlab::params
